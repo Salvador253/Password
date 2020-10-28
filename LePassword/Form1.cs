fd9418c5a15d0548005df36eba4c9c9bd036f056
luis2652 <71356747+luis2652@users.noreply.github.com>
2020-10-28 00:02:30 -0600

    CAMBIOS
    realize cambios en el codigo para login y cuadro de texto asi agrege la ventana de ingreso y sus botones y cuadros de texto

--- LePassword/Form1.cs
+++ LePassword/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace LePassword
 {
@@ -15,6 +16,66 @@ namespace LePassword
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Registrar()
+        {
+            string connect = "datasource=localhost;post=3306;username=root;password=;database=sistema";
+            string query = "SELECT * from user where username = '" + textBox1 + "' AND password = '" + textBox2 + "' ";
+            MySqlConnection databaseConnection = new MySqlConnection(connect);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    MessageBox.Show("Bienvenido al sistema de waldos");
+                    databaseConnection.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Ingreso fallido intente nuevamente");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Registrar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
